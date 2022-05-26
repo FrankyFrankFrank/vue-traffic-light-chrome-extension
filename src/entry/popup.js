382ee727c321f4b44c98d133f6c71c75e0e59b61
FrankyFrankFrank <adam@webadamfrank.com>
2022-05-26 18:40:52 -0400

    remove stuff from popup

--- src/entry/popup.js
+++ src/entry/popup.js
@@ -1,26 +1,6 @@
 import { createApp } from 'vue'
-import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-
 import App from '../view/popup.vue'
 
-const firebaseConfig = {
-  apiKey: process.env.VUE_APP_APIKEY,
-  authDomain: process.env.VUE_APP_AUTHDOMAIN,
-  projectId: process.env.VUE_APP_PROJECTID,
-  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
-  appId: process.env.VUE_APP_APPID,
-  measurementId: process.env.VUE_APP_MEASUREMENTID,
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-const auth = getAuth(app);
-console.log(auth)
-// const provider = new GoogleAuthProvider();
 
 
 createApp(App).mount('#app')
