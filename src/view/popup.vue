e6e64a08b8c8006787fe915d427fa13d1b5b20c5
FrankyFrankFrank <adam@webadamfrank.com>
2022-05-26 19:21:28 -0400

    classf

--- src/view/popup.vue
+++ src/view/popup.vue
@@ -1,19 +1,42 @@
 <template>
   <div class="main_app">
-    <h1>Hello {{msg}}</h1>
+    <h1>Hello {{ msg }}</h1>
+    <button @click="foo">Foo</button>
   </div>
 </template>
 
 <script>
+import { initializeApp } from "firebase/app";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
+
 export default {
   name: 'popupView',
-  data () {
+  data() {
     return {
       msg: 'popup'
     }
+  },
+  methods: {
+    foo: async () => {
+      const firebaseConfig = {
+        apiKey: process.env.APIKEY,
+        authDomain: process.env.AUTHDOMAIN,
+        projectId: process.env.PROJECTID,
+        storageBucket: process.env.STORAGEBUCKET,
+        messagingSenderId: process.env.MESSAGINGSENDERID,
+        appId: process.env.APPID,
+        measurementId: process.env.MEASUREMENTID,
+      };
+      const app = initializeApp(firebaseConfig);
+      const db = getFirestore(app);
+
+      const querySnapshot = await getDocs(collection(db, "teams"));
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+      });
+    }
   }
 }
-
 </script>
 
 <style>
