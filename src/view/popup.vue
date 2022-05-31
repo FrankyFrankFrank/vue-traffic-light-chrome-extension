ec455dfe54925454e0e92edf67251e08a6e09f04
FrankyFrankFrank <adam@webadamfrank.com>
2022-05-31 16:58:05 -0400

    adds copy to clipboard button

--- src/view/popup.vue
+++ src/view/popup.vue
@@ -6,6 +6,13 @@
       <h1 class="text-lg text-center font-bold tracking-widest mb-4">
         <span class="text-slate-400">Team</span>
         {{ loadedTeam }}
+        <button @click="copyTeamNameToClipboard(loadedTeam)">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+            stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+          </svg>
+        </button>
       </h1>
       <div class="mb-4 border-b pb-4">
         <div v-for="member in teamMembers" :key="member" class="mb-2">
@@ -154,6 +161,10 @@ function setColor(memberId, color) {
   updateDoc(memberRef, {
     color
   })
+}
+
+function copyTeamNameToClipboard(teamName) {
+  navigator.clipboard.writeText(teamName);
 }
 
 </script>
